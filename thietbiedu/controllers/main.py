f5c51474a32b7a1ee4801994364c298edd017289
LvQuy <quyseo.ictu@gmail.com>
2022-10-31 14:04:55 +0700

    add confirm gg search

---
+++ thietbiedu/controllers/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+
+class Thietbiedu(http.Controller):
+
+    @http.route(['/google51aebbe99d6bf534.html'], auth='public', website=True)
+    def google_search(self, **kw):
+        return request.render('quang_cao.google51aebbe99d6bf534')
